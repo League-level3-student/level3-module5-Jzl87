c72e461e9d722c303c59fef63618fb163fab8329
smili <smili@LAPTOP-AVKP4CIO.attlocal.net>
2025-03-30 13:35:04 -0700

    yruj

--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -18,12 +18,27 @@ class RecursionMathTest {
     @Test
     public void testDivision() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+    System.out.println(RecursionMath.recursiveDivision(36, 3));
+        assertEquals(12,  RecursionMath.recursiveDivision(36, 3));
+        System.out.println(RecursionMath.recursiveDivision(36, 18));
+        assertEquals(2,  RecursionMath.recursiveDivision(36, 18));
+        System.out.println(RecursionMath.recursiveDivision(56,1));
+        assertEquals(56,  RecursionMath.recursiveDivision(56, 1));
     }
 
     @Test 
     public void testPower() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+    	
+
+
+    	assertEquals(36,  RecursionMath.recursivePower(6,2));
+    	
+    	
+    
+    	assertEquals(65536,  RecursionMath.recursivePower(2,16));
+    	
+    	
+    	assertEquals(125,  RecursionMath.recursivePower(5, 3));
     }
 }
